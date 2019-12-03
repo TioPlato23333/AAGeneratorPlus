d63da3923795ea91517eff5fae30d7a491b2c401
TioPlato23333 <noemail@here.com>
2019-12-03 18:39:33 +0800

    Text drawing test.

--- AAGenerator/generator.py
+++ AAGenerator/generator.py
@@ -10,11 +10,33 @@
 
 import cv2
 import numpy as np
+import os
+from PIL import Image, ImageDraw, ImageFont
 
 def generate(string, param):
-  character = 255 * np.ones(param.CHARACTER_SIZE, dtype='uint8')
-  cv2.putText(character, '}', (0, param.CHARACTER_SIZE[0]), \
-    cv2.FONT_HERSHEY_SIMPLEX, 0.5, param.CHARACTER_COLOR)
-  print character
-  cv2.imshow('sample', character)
-  cv2.waitKey()
+  # character = 255 * np.ones(param.CHARACTER_SIZE, dtype='uint8')
+  # cv2.putText(character, '}', (0, param.CHARACTER_SIZE[0]), \
+  #   cv2.FONT_HERSHEY_SIMPLEX, 0.5, param.CHARACTER_COLOR)
+  # print character
+  # cv2.imshow('sample', character)
+  # cv2.waitKey()
+  # get an image
+  current_path, _ = os.path.split(os.path.abspath(__file__))
+  base = Image.open(os.path.join(current_path, '../test_asset/fate_astolfo.jpg')).convert('RGBA')
+
+  # make a blank image for the text, initialized to transparent text color
+  txt = Image.new('RGBA', base.size, (255,255,255,0))
+
+  # get a font
+  fnt = ImageFont.truetype(os.path.join('../resources/SFNSText.ttf'), 100)
+  # get a drawing context
+  d = ImageDraw.Draw(txt)
+
+  # draw text, half opacity
+  d.text((10,10), "Hello", font=fnt, fill=(0,255,255,128))
+  # draw text, full opacity
+  d.text((10,160), "World", font=fnt, fill=(0,255,255,255))
+
+  out = Image.alpha_composite(base, txt)
+
+  out.show()
